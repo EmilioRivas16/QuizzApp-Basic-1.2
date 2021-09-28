5c6b58aa84959e89f91a8e4e4cdf92e00392a357
Daniel López <lopezdaniele114@gmail.com>
2021-09-28 14:51:53 -0500

    Activity Main 1 y 2 actualizados, Themes actualizados y Vistas actualizadas

--- app/src/main/java/com/example/quizappbasic/MainActivity.kt
+++ app/src/main/java/com/example/quizappbasic/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.quizappbasic
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,16 +10,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnjugar: Button = findViewById(R.id.boton_jugar)
+        val btnPlay: Button = findViewById(R.id.btn_play)
 
-        btnjugar.setOnClickListener {
+        btnPlay.setOnClickListener {
             val intent:Intent = Intent(this, MainActivity3::class.java)
             startActivity(intent)
         }
 
-        val btnopciones: Button = findViewById(R.id.boton_opciones)
+        val btnSettings: Button = findViewById(R.id.btn_settings)
 
-        btnopciones.setOnClickListener {
+        btnSettings.setOnClickListener {
             val intent:Intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }

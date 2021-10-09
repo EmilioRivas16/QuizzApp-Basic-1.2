d05d592e466121f507bfc6a56c5f4b35bf4d1062
ItsDaniel468 <lopezdaniele114@gmail.com>
2021-10-09 15:28:17 -0500

    Manejadores para el Activity 3 casi implementados al cien.
    Faltan algunos manejadores para los botones y para mostrar las preguntas de forma aleatoria por cada partida.

--- app/src/main/java/com/example/quizappbasic/Question.kt
+++ app/src/main/java/com/example/quizappbasic/Question.kt
@@ -1,8 +1,9 @@
 package com.example.quizappbasic
 
 data class Question(
+    val theme: String,
     val question: String,
     val answers: Array<String>,
-    var answered: Boolean = false,
-    var correct: Boolean = false
+    var answered: Boolean,
+    var correct: Boolean
 )
